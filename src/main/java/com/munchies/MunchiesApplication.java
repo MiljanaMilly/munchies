4cba0d48124187175ccf4f4c3346ec62811a8c30
Miljana Milly <miljana.milly@gmail.com>
2018-10-11 16:21:44 +0200

    Initial commit

---
+++ src/main/java/com/munchies/MunchiesApplication.java
@@ -0,0 +1,12 @@
+package com.munchies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunchiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MunchiesApplication.class, args);
+    }
+}
