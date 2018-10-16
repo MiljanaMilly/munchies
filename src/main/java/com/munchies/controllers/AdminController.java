6858f522d23cee823e37a3760843bd159b019563
Miljana Milly <miljana.milly@gmail.com>
2018-10-16 16:59:02 +0200

    Initial, role-based structure done! Yaay!!

--- src/main/java/com/munchies/controllers/AdminController.java
+++ src/main/java/com/munchies/controllers/AdminController.java
@@ -1,6 +1,8 @@
 package com.munchies.controllers;
 
+import com.munchies.model.Restaurant;
 import com.munchies.model.User;
+import com.munchies.services.RestaurantService;
 import com.munchies.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,10 +20,22 @@ public class AdminController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RestaurantService restaurantService;
+
     @GetMapping("/zaposleni")
     public ModelAndView zaposleniPage(ModelAndView model) {
         List<User> usersList = userService.getAllUsers();
         model.addObject("usersList", usersList);
+        model.setViewName("zaposleni");
+        return model;
+    }
+
+    @GetMapping("/restorani")
+    public ModelAndView restoraniPage(ModelAndView model) {
+        List<Restaurant> restList = restaurantService.getAllRest();
+        model.addObject("restList", restList);
+        model.setViewName("restorani");
         return model;
     }
 
